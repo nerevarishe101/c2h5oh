5c6c398de14ad7f4bcbed38d3bdb98854455a760
i.smorodin <smorodin.it@gmail.com>
2025-03-23 22:28:16 +0300

    add stringDeclination with tests

--- packages/utils/src/lib/utils.ts
+++ packages/utils/src/lib/utils.ts
@@ -1,3 +1,30 @@
-export function utils(): string {
-  return 'utils';
-}
+/**
+ * Склоняет слово в зависимости от числового значения.
+ *
+ * Пример:
+ *
+ * ```js
+ * `Длина поля не может быть меньше чем ${stringDeclination(2, ['символ', 'символа', 'символов'])}`
+ * ```
+ *
+ * В результате преобразуется в строку:
+ *
+ * ```js
+ * `Длина поля не может быть меньше чем 2 символа`
+ * ```
+ * @param num - число
+ * @param labels - массив из трёх строк
+ */
+export const stringDeclination = (
+  num: number,
+  labels: [string, string, string]
+): string => {
+  const cases = [2, 0, 1, 1, 1, 2];
+  return (
+    num.toString() +
+    ' ' +
+    labels[
+      num % 100 > 4 && num % 100 < 20 ? 2 : cases[num % 10 < 5 ? num % 10 : 5]
+    ]
+  );
+};
