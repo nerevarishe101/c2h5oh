b2ca81699e971aee7f971d7a5d80408581d7587a
i.smorodin <smorodin.it@gmail.com>
2025-03-23 20:09:30 +0300

    base eslint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,79 @@
+import nx from '@nx/eslint-plugin';
+import imports from 'eslint-plugin-import';
+import sortExports from 'eslint-plugin-sort-exports';
+
+// eslint-disable-next-line import/no-default-export
+export default [
+  ...nx.configs['flat/base'],
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
+  {
+    ignores: ['**/dist'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    files: [
+      '**/*.ts',
+      '**/*.tsx',
+      '**/*.js',
+      '**/*.jsx',
+      '**/*.cjs',
+      '**/*.mjs',
+    ],
+    plugins: {
+      import: imports,
+      'sort-exports': sortExports,
+    },
+    // Override or add rules here
+    rules: {
+      // Настройка импортов
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      'import/no-default-export': 'error',
+      'import/prefer-default-export': 'off',
+
+      //   Настройка экспортов
+      'sort-exports/sort-exports': [
+        'error',
+        {
+          sortDir: 'asc',
+          ignoreCase: true,
+          sortExportKindFirst: 'type',
+        },
+      ],
+    },
+  },
+];
