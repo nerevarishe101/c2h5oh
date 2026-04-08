b6b1bc91df70c745b93056229bf6e4841b5d0a5f
i.t.smorodin <smorodin.it@gmail.com>
2026-04-08 16:47:57 +0300

    base configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,7 @@
 import nx from '@nx/eslint-plugin';
+import imports from 'eslint-plugin-import';
+import perfectionist from 'eslint-plugin-perfectionist';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 // eslint-disable-next-line import/no-default-export
 export default [
@@ -19,18 +22,42 @@ export default [
       '@nx/enforce-module-boundaries': [
         'error',
         {
-          enforceBuildableLibDependency: true,
           allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
           depConstraints: [
             {
-              sourceTag: '*',
               onlyDependOnLibsWithTags: ['*'],
+              sourceTag: '*',
             },
           ],
+          enforceBuildableLibDependency: true,
         },
       ],
     },
   },
+  // --- Perfectionist export order from index files
+  {
+    files: ['**/index.ts'],
+    plugins: {
+      perfectionist,
+    },
+    rules: {
+      'perfectionist/sort-exports': [
+        'error',
+        {
+          partitionByComment: true,
+          partitionByNewLine: true,
+        },
+      ],
+      'perfectionist/sort-named-exports': [
+        'error',
+        {
+          partitionByComment: true,
+          partitionByNewLine: true,
+        },
+      ],
+    },
+  },
+
   {
     files: [
       '**/*.ts',
@@ -39,41 +66,71 @@ export default [
       '**/*.jsx',
       '**/*.cjs',
       '**/*.mjs',
+      '!**/vite.config.ts',
     ],
-    plugins: {},
+    plugins: {
+      import: imports,
+      perfectionist,
+      'unused-imports': unusedImports,
+    },
     // Override or add rules here
-    rules: {
-      // Настройка импортов
-      'import/order': [
+    rules: (() => {
+      const sortInterfacesAndObjectTypesConfiguration = [
         'error',
         {
           groups: [
-            'builtin',
-            'external',
-            'internal',
-            'parent',
-            'sibling',
-            'index',
+            'index-signature',
+            'required-property',
+            'optional-property',
+            { newlinesBetween: 1 },
+            'required-method',
+            'optional-method',
           ],
-          'newlines-between': 'always',
-          alphabetize: {
-            order: 'asc',
-            caseInsensitive: true,
-          },
         },
-      ],
-      'import/no-default-export': 'error',
-      'import/prefer-default-export': 'off',
+      ];
 
-      //   Настройка экспортов
-      'sort-exports/sort-exports': [
-        'error',
-        {
-          sortDir: 'asc',
-          ignoreCase: true,
-          sortExportKindFirst: 'type',
-        },
-      ],
-    },
+      return {
+        // Отключить дублирующееся предупреждение
+        '@typescript-eslint/no-unused-vars': 'off',
+        'import/no-default-export': 'error',
+
+        'import/prefer-default-export': 'off',
+        'perfectionist/sort-heritage-clauses': 'error',
+        // -- Perfectionist configuration
+        // ---- Perfectionist imports
+        'perfectionist/sort-imports': [
+          'error',
+          {
+            partitionByComment: true,
+          },
+        ],
+        // ---- Perfectionist sorts
+        'perfectionist/sort-interfaces':
+          sortInterfacesAndObjectTypesConfiguration,
+        'perfectionist/sort-intersection-types': 'error',
+        'perfectionist/sort-named-imports': 'error',
+
+        'perfectionist/sort-object-types':
+          sortInterfacesAndObjectTypesConfiguration,
+        'perfectionist/sort-objects': [
+          'error',
+          {
+            groups: ['property', { newlinesBetween: 1 }, 'method'],
+          },
+        ],
+        'perfectionist/sort-union-types': 'error',
+        // -- Other plugins
+        'unused-imports/no-unused-imports': 'error',
+        'unused-imports/no-unused-vars': [
+          'warn',
+          {
+            args: 'after-used',
+            argsIgnorePattern: '^_',
+            vars: 'all',
+            varsIgnorePattern: '^_',
+          },
+        ],
+      };
+    })(),
   },
 ];
